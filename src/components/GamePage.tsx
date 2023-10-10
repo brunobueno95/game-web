8b62d458190760cc680e01e5741c9be95b980aac
Bruno Bueno <brunobernardobueno@gmail.com>
2023-10-10 21:20:00 +0200

    Implement Game Page

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -1,6 +1,17 @@
-import { Button, Flex, Heading, Spinner } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Heading,
+  Spinner,
+  Image,
+  Text,
+  HStack,
+  Box,
+} from "@chakra-ui/react";
 
 import useIndividualGame from "../hooks/useIndividualGame";
+import getCroppedImageUrl from "../services/image-url";
+import removeHtmlTags from "../services/remove-htmlTag";
 
 interface Props {
   gameId: number;
@@ -14,17 +25,56 @@ const GamePage = ({ gameId, openClosePage }: Props) => {
   if (error) return <div>Error: {error}</div>;
   if (!data) return null;
 
-  const consoleLogs = () => {
-    console.log(data);
-  };
+  if (data[0])
+    return (
+      <>
+        <Button
+        marginTop={5}
+          marginLeft={5}
+          marginBottom={0}
+          textAlign={"right"}
+         
+          colorScheme={"red"}
+          onClick={() => openClosePage(false)}
+        >
+          X
+        </Button>
+        <Flex
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          paddingBottom={20}
+        >
+          <Heading fontSize={"5xl"} my={20}>
+            {data[0].name}
+          </Heading>
+          <Image
+            borderRadius={"10px"}
+            width={{ base: "100%", md: "500px" }}
+            height={"auto"}
+            src={getCroppedImageUrl(data[0].background_image)}
+          />
 
-  return (
-    <Flex>
-      <Heading>{gameId}</Heading>
-      <Button onClick={() => openClosePage(false)}>X</Button>
-      <Button onClick={consoleLogs}>CONSOLO.LOG(DATA)</Button>
-    </Flex>
-  );
+          <Box
+            display={"flex"}
+            flexDirection={"column"}
+            justifyContent={"center"}
+            alignItems={'center'}
+            boxSize={"60%"}
+            marginTop={10}
+          >
+            {" "}
+            <Text fontSize={"2xl"}> {removeHtmlTags(data[0].description)}</Text>
+            <HStack marginTop={4}>
+              <Text fontSize={"xl"}>Website: </Text>
+              <Button color={"#16BCE6"} variant={"link"} fontSize={"lg"}>
+                {data[0].website}
+              </Button>
+            </HStack>
+          </Box>
+        </Flex>
+      </>
+    );
 };
 
 export default GamePage;
---
+++ src/services/remove-htmlTag.ts
@@ -0,0 +1,6 @@
+const removeHtmlTags = (html: string) => {
+  const tempDiv = document.createElement("div");
+  tempDiv.innerHTML = html;
+  return tempDiv.textContent || tempDiv.innerText || "";
+};
+export default removeHtmlTags;
