1f971d4ec140c5975fe180d6c0ccd1854a083f40
Bruno Bueno <brunobernardobueno@gmail.com>
2023-10-10 03:54:56 +0200

    display items

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Game } from '../hooks/useGames'
-import { Card, Image, CardBody, Heading} from '@chakra-ui/react'
+import { Card, Image, CardBody, Heading, } from '@chakra-ui/react'
+import PlatformIcon from './navbar/PlatformIcon'
+
 
 interface Props {
     game: Game
@@ -11,7 +13,8 @@ const GameCard = ({game}:Props) => {
    <Card borderRadius={10} overflow={'hidden'} maxHeight={"300px"}>
     <Image height={"70%"} src={game.background_image}/>
     <CardBody height={"30%"}>
-        <Heading fontSize={'2xl'} >{game.name}</Heading>
+        <Heading fontSize={'xl'} >{game.name}</Heading>
+      <PlatformIcon platforms={game.parent_platforms.map(p=>p.platform)}/>
     </CardBody>
    </Card>
   )
---
+++ src/components/navbar/PlatformIcon.tsx
@@ -0,0 +1,46 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons/lib/esm/iconBase";
+import { Platform } from "../../hooks/useGames";
+import { HStack, Icon, } from "@chakra-ui/react";
+
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIcon = ({ platforms }: Props) => {
+    const iconMap:{[key:string] : IconType} = {
+       pc: FaWindows,
+       playstation: FaPlaystation,
+       xbox: FaXbox,
+       nintendo: SiNintendo,
+       mac: FaApple,
+       linux: FaLinux,
+       ios:MdPhoneIphone,
+       web:BsGlobe,
+       androind:FaAndroid
+
+    }
+  return (
+    <div>
+      <HStack  marginTop={3}>
+        {" "}
+        {platforms.map((p) => (
+          <Icon color={'gray.500'} as={iconMap[p.slug]} />
+        ))}
+      </HStack>
+    </div>
+  );
+};
+
+export default PlatformIcon;
