fd533c856cc5122d7cfff1aadefd4c9a6bc12b0f
Bruno Bueno <brunobernardobueno@gmail.com>
2023-10-19 17:25:45 +0200

    inifinity scrolling

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,35 +5,73 @@ import GameCardSkeleton from "./gameCard/GameCardSkeleton";
 import GameCardContainer from "./gameCard/GameCardContainer";
 
 import { GameQuery } from "../App";
+import React, { useEffect } from "react";
 
 interface Props {
   gameQuery: GameQuery;
   gameIDclickFunction: (id: number) => void;
 }
 const GameGrid = ({ gameQuery, gameIDclickFunction }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery);
+  const loadMorePosts = () => {
+    if (isFetchingNextPage) return; // Don't fetch if a request is already in progress
+    fetchNextPage();
+  };
+
+  const handleScroll = () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+
+    // Check if the user has scrolled to the bottom (or near the bottom)
+    if (scrollTop + clientHeight >= scrollHeight - 100) {
+      loadMorePosts();
+    }
+  };
+
+  // Attach the scroll event listener
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const {
+    data,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    fetchNextPage,
+  } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
   if (error) return <Text>{error.message}</Text>;
   return (
-    <SimpleGrid
-      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-      spacing={8}
-      marginTop={25}
-    >
-      {data &&
-        data.results.length > 0 &&
-        data.results.map((g) => (
-          <GameCardContainer key={g.id}>
-            <GameCard gameIDclickFunction={gameIDclickFunction} game={g} />
-          </GameCardContainer>
+    <>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={8}
+        marginTop={25}
+      >
+        {data?.pages.map((page, i) => (
+          <React.Fragment key={i}>
+            {page.results.map((g) => (
+              <GameCardContainer key={g.id}>
+                <GameCard gameIDclickFunction={gameIDclickFunction} game={g} />
+              </GameCardContainer>
+            ))}
+          </React.Fragment>
         ))}
-      {isLoading &&
-        skeletons.map((s, i) => (
-          <GameCardContainer key={s}>
-            <GameCardSkeleton key={i} />
-          </GameCardContainer>
-        ))}
-    </SimpleGrid>
+
+        {isLoading &&
+          skeletons.map((s, i) => (
+            <GameCardContainer key={s}>
+              <GameCardSkeleton key={i} />
+            </GameCardContainer>
+          ))}
+      </SimpleGrid>
+      {/* {hasNextPage && (
+        <Button onClick={()=>fetchNextPage()}>{isFetchingNextPage ? "Loading..." : "Load More"}</Button>
+      )} */}
+    </>
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { Platform } from "./usePlatforms";
-import APIClient from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
 
 
 const apiClient = new APIClient<Game>("/games");
@@ -16,16 +16,24 @@ export interface Game {
 
 const useGames = (gameQuery: GameQuery) =>
 
-  useQuery({
-    queryKey: ["games", gameQuery],
-    queryFn: () => apiClient.getAll({
+useInfiniteQuery<FetchResponse<Game>, Error>({
+  queryKey:['games', gameQuery],
+  queryFn: ({ pageParam = 1 }) => {
+    return apiClient.getAll({
       params: {
         genres: gameQuery.genre?.id,
         parent_platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
         search: gameQuery.searchText,
+        page: pageParam,
       },
-    })
-  });
+    });
+  }, 
+  initialPageParam: 1,
+  getNextPageParam: (lastPage, allPages) => {
+  
+    return lastPage.next ?  allPages.length + 1 : undefined;
+  },
+})
 
 export default useGames;
